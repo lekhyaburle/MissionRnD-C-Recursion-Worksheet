636d32d573c08fb07219095972c82f976318fe5f
lekhyaburle <lekhyaburle@missionrnd.com>
2016-04-26 16:01:56 +0530

    Submit Commit

--- src/Steps.cpp
+++ src/Steps.cpp
@@ -23,8 +23,16 @@
 */
 #include "stdafx.h"
 
-
+int f(int n)
+{
+	if (n == 0) 
+		return 0;
+	else if (n == 1) 
+		return 1;
+	else 
+		return (f(n - 1) + f(n - 2));
+}
 int get_steps(int s)
 {
-	return 0;
+	return f(s+1);
 }
